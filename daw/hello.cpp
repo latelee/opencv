fdcbf6734ac0f7251749d4caf1b3270501bac0cc
Late Lee <li@latelee.org>
2017-09-02 12:35:18 +0800

    添加rect到RotatedRect的转换。
    Signed-off-by: Late Lee <li@latelee.org>

--- daw/hello.cpp
+++ daw/hello.cpp
@@ -7,6 +7,80 @@ opencv画矩形示例：
 //#include<opencv/CvxText.h>
 using namespace cv;
 
+void draw_rect(Mat& imaged)
+{
+    // 方法1：指定坐标，宽高
+    Rect rect;
+    // 左上角坐标
+    rect.x = 0;
+    rect.y = 0;
+    // 宽高
+    rect.width = 100;
+    rect.height = 100;
+    
+    printf("draw rectangle...\n");
+    rectangle(imaged, rect, Scalar(0, 255, 255), 2);
+
+    // 方法2：指定中心坐标，宽高
+    Point2f points[4];
+    RotatedRect rrect;
+    // 中心坐标
+    rrect.center.x = 100;
+    rrect.center.y = 100;
+    // 矩形宽高
+    rrect.size.width = 20;
+    rrect.size.height = 20;
+
+    rrect.points(points);
+
+    printf("draw RotatedRect...\n");
+    for (int j = 0; j < 4; j++)
+    {
+        line(imaged, points[j], points[(j+1)%4], Scalar(0, 0, 255), 2);
+    }
+
+    //CvxText text("simsun.ttc");
+
+    putText(imaged, "This is text 中文", cv::Point(0, 150), 1, 1, cvScalar(0, 0, 255));
+}
+
+void rect2rrect(Rect& rect, RotatedRect& rrect)
+{
+    // 中心坐标
+    rrect.center.x = rect.x + rect.width/2;
+    rrect.center.y = rect.y + rect.height/2;
+    // 矩形宽高
+    rrect.size.width = rect.width;
+    rrect.size.height = rect.height;
+}
+
+// 从rect转换成RotatedRect
+void draw_rect2(Mat& imaged)
+{
+    Rect rect;
+    // 左上角坐标
+    rect.x = 100;
+    rect.y = 0;
+    // 宽高
+    rect.width = 100;
+    rect.height = 100;
+    
+    Point2f points[4];
+    RotatedRect rrect;
+    
+    
+    rect2rrect(rect, rrect);
+
+    rrect.points(points);
+
+    printf("draw RotatedRect...\n");
+    for (int j = 0; j < 4; j++)
+    {
+        line(imaged, points[j], points[(j+1)%4], Scalar(0, 0, 255), 2);
+    }
+    
+}
+
 int main(int argc, char* argv[])
 {
     if ( argc != 2 )
@@ -21,7 +95,7 @@ int main(int argc, char* argv[])
         printf("No image data \n");
         return -1;
     }
-    printf("res: %dx%d\n", image.cols, image.rows);
+    printf("got res: %dx%d\n", image.cols, image.rows);
     // 没有界面的话，下面几句不需要
     //namedWindow("Display Image", WINDOW_AUTOSIZE );
     //imshow("Display Image", image);
@@ -32,33 +106,10 @@ int main(int argc, char* argv[])
     // 拷贝一份，不影响原图
     image.copyTo(imaged);
 
-    // 方法1：指定坐标，宽高
-    Rect rect;
-    rect.x = 0;
-    rect.y = 0;
-    rect.width = 100;
-    rect.height = 100;
-    rectangle(imaged, rect, Scalar(0, 255, 255), 2);
 
-    // 方法2：指定中心坐标，宽高
-    Point2f points[4];
-    RotatedRect rrect;
-    rrect.center.x = 100;
-    rrect.center.y = 100;
-    rrect.size.width = 20;
-    rrect.size.height = 20;
-
-    rrect.points(points);
-    for (int j = 0; j < 4; j++)
-    {
-        line(imaged, points[j], points[(j+1)%4], Scalar(0, 0, 255), 2);
-    }
-
-
-    //CvxText text("simsun.ttc");
-
-    putText(imaged, "This is text 中文", cv::Point(0, 150), 2, 1, cvScalar(0, 0, 255));
-
+    //draw_rect(imaged);
+    draw_rect2(imaged);
+    
     imwrite("foo_draw.jpg", imaged);
 
     return 0;
